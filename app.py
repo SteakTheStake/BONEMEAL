8bc86b812e0543f86789999f91775b5505636de1
Gabe Corr <gabecorr25@gmail.com>
2024-02-10 18:52:15 -0800

    version 1.8

--- app.py
+++ app.py
@@ -2,9 +2,8 @@
 import os
 import tempfile, subprocess
 
-from werkzeug.utils import secure_filename, send_from_directory
-from flask import Flask, request, render_template
-from flask import session, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import Flask, request, render_template, session, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'summit_mc_xyz'
@@ -18,11 +17,8 @@ def home():
 @app.route('/resize', methods=['GET', 'POST'])
 def resize():
     if request.method == 'POST':
-        # Get uploaded files and resize percentage
         uploaded_files = request.files.getlist("files")
         percentage = request.form['percentage']
-
-        # Validate percentage
         try:
             percentage = float(percentage)
             if not 0 < percentage <= 100:
@@ -30,41 +26,32 @@ def resize():
         except ValueError as e:
             return render_template('custom/resize.html', output=str(e))
 
-        # Process each uploaded file
         output_messages = []
         for file in uploaded_files:
             if file:
-                # Save the file temporarily
                 filename = secure_filename(file.filename)
                 file_path = os.path.join(tempfile.gettempdir(), filename)
                 file.save(file_path)
-
-                # Call the resize script as a subprocess
                 process = subprocess.run(
                     ['python', 'static/py/resize-script.py', file_path, str(percentage)],
                     capture_output=True, text=True)
+                print(f"Process Error: {process.stderr}")
+                print(f"Process Output: {process.stdout}")
                 output_messages.append(process.stdout)
 
-        # Combine all messages for output
         output = "\n".join(output_messages)
+        print(f"Final Output: {output}")  # Debugging line
         session['resize_output'] = output
+        print(f"Session Output: {session['resize_output']}")  # Debugging line
         return redirect(url_for('resize_result'))
 
-        # Redirect to the resize-result route
-
-    # For a GET request, just render the template
     return render_template('custom/resize.html')
 
 
 @app.route('/resize-result', methods=['GET'])
 def resize_result():
-    if request.method == 'POST':
-        # In case of POST, retrieve data from the session or form
-        output = session.get('resize_output', 'No data available')
-    else:
-
-        output = 'No data available'
-
+    output = session.get('resize_output', 'No data available')
+    print(f"Resize Result Output: {output}")  # Debugging line
     return render_template('custom/resize-result.html', output=output)
 
 
