75f1969bd2a64119ecd1c2a54abc4a2f1a63bf4c
Gabe Corr <gabecorr25@gmail.com>
2024-03-01 13:08:20 -0800

    updated css errors

--- static/js/sound.js
+++ static/js/sound.js
@@ -1,10 +1,3 @@
-
-//let hoverAudioClip = new Audio('/static/sound/clicksound.mp3');
-
-//document.getElementById('hoverSound').addEventListener('mouseenter', function() {
-//    hoverAudioClip.play();
-//});
-
 let clickAudioClip = new Audio('/static/sound/hoversound.mp3');
 document.getElementById('clickSound').addEventListener('mousedown', function(button) {
     clickAudioClip.play();
