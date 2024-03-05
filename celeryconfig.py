57db8299332cb622c816140ab403bb11219c2b68
SteakTheStake <gabecorr25@gmail.com>
2024-03-04 22:58:38 -0800

    version 5.3

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,10 +1,10 @@
 ## Broker settings.
-broker_url = 'redis://162.33.23.205:6379/1'
+CELERY_BROKER_URL = 'redis://162.33.23.205:6379/1'
 
 # List of modules to import when the Celery worker starts.
 imports = ('app.tasks',)
 
 ## Using the database to store task state and results.
-result_backend = 'db+sqlite:///results.db'
+CELERY_RESULT_BACKEND = 'redis://162.33.23.205:6379/1'
 
 task_annotations = {'tasks.add': {'rate_limit': '10/s'}}
