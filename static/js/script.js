7fe95c84ffa8a6129a0425ddd744ba36d562571b
Gabe Corr <gabecorr25@gmail.com>
2024-03-04 19:57:06 -0800

    version 5.0

--- static/js/script.js
+++ static/js/script.js
@@ -14,3 +14,10 @@ function delay (URL) {
 document.getElementById("go-back").addEventListener("click", () => {
     history.back();
 });
+
+const gradient = document.querySelector(".gradient");
+
+function onMouseMove(event) {
+    gradient.style.backgroundImage = 'radial-gradient(at ' + event.clientX + 'px ' + event.clientY + 'px, rgba(33, 15, 75, 0.75) 0, #120431 70%)';
+}
+document.addEventListener("mousemove", onMouseMove);
