08013fba012f22c2bea20557047a27473b8094bf
SteakTheStake <gabecorr25@gmail.com>
2024-03-04 22:20:59 -0800

    version 5.1

--- static/js/script.js
+++ static/js/script.js
@@ -10,14 +10,3 @@ document.addEventListener('DOMContentLoaded', function () {
 function delay (URL) {
     setTimeout( function() { window.location = URL }, 250 );
 }
-
-document.getElementById("go-back").addEventListener("click", () => {
-    history.back();
-});
-
-const gradient = document.querySelector(".gradient");
-
-function onMouseMove(event) {
-    gradient.style.backgroundImage = 'radial-gradient(at ' + event.clientX + 'px ' + event.clientY + 'px, rgba(33, 15, 75, 0.75) 0, #120431 70%)';
-}
-document.addEventListener("mousemove", onMouseMove);
