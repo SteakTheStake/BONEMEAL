925a75513779d6259867d0d707a46f52639f1a00
SteakTheStake <gabecorr25@gmail.com>
2024-03-05 17:16:59 -0800

    version 5.8

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,10 +1,10 @@
 ## Broker settings.
-CELERY_BROKER_URL = 'redis://bonemeal.summitmc.xyz:6379/1'
+CELERY_BROKER_URL = 'redis://localhost:6379/1'
 
 # List of modules to import when the Celery worker starts.
 imports = ('app.py',)
 
 ## Using the database to store task state and results.
-result_backend = 'redis://bonemeal.summitmc.xyz:6379/1'
+result_backend = 'redis://localhost:6379/1'
 
 task_annotations = {'tasks.add': {'rate_limit': '10/s'}}
